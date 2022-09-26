90a0815e9675a058a60fc589b37011bbc117646f
Maueeza Bhatkar <90929270+maueeza@users.noreply.github.com>
2022-09-26 18:11:02 +0530

    feat : added validation
    Added bill and cash validation

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-var billAmount = document.querySelector("#bill-amt");
-console.log(billAmount);
+const inpBillAmount = document.getElementById("bill-amt");
+const billAmount = inpBillAmount.value;
+//console.log(billAmount);
+var inpCash = document.querySelector("#cash-note");
+var cashAmt = inpCash.value;
+//console.log(cashAmt);
+
+var chkbtn = document.querySelector("#check-btn");
+function chkBtnHandler()
+{
+    console.log("Bill Amount is : "+billAmount);
+    console.log("Cash Amount is : " +cashAmt);
+    if(billAmount>0)
+    {
+       if(cashAmt>=billAmount)
+       {
+         console.log("Valid code")
+       }
+       else
+       {
+        console.log("Cash Amount should be greater than or equal to Bill Amount");
+       }
+    }
+    else
+    {
+        console.log("Invalid Bill Amount");
+    }
+    
+}
+
+chkbtn.addEventListener("click",chkBtnHandler);
+
